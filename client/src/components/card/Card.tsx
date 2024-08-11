b15e606a22fae6c504a558152b76563075e52492
Ruslan <110040060+Deadusik@users.noreply.github.com>
2024-08-11 05:23:49 +0300

    add labels for card

--- client/src/components/card/Card.tsx
+++ client/src/components/card/Card.tsx
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react'
 import styles from '../../styles/components/card/Card.module.scss'
 import testImgSrc from '../../assets/imgs/test/avatar.png'
 import { SPACE } from '../../utils/constants'
+import useNormilize from '../../hooks/useNormalize'
 
 const Card: React.FC = () => {
     const [isFrontSide, setIsFrontSide] = useState(true)
@@ -10,8 +11,17 @@ const Card: React.FC = () => {
     const [isMouseDown, setIsMouseDown] = useState(false)
 
     const [primaryCursorPoint, setPrimaryCursorPoint] = useState({ x: 0, y: 0 })
+    const [moveOffset, setMoveOffset] = useState({ x: 0, y: 0 })
+    const [labelOpacity, setLabelOpacity] = useState(0)
 
     const cardRef = useRef<HTMLDivElement>(null)
+    const gotItLabelRef = useRef<HTMLDivElement>(null)
+    const studyAgainLabelRef = useRef<HTMLDivElement>(null)
+    const deleteLabelRef = useRef<HTMLDivElement>(null)
+
+    const GOT_IT = 100
+    const STUDY_AGAIN = -100
+    const DELETE_OFFSET = 100
 
     const rotateCard = (event: MouseEvent) => {
         if (cardRef.current) {
@@ -21,9 +31,18 @@ const Card: React.FC = () => {
 
     const moveCard = (event: MouseEvent) => {
         if (cardRef.current) {
-            cardRef.current.style.left = `${event.clientX - primaryCursorPoint.x}px`
-            cardRef.current.style.top = `${event.clientY - primaryCursorPoint.y}px`
+            const offsetX = event.clientX - primaryCursorPoint.x
+            const offsetY = event.clientY - primaryCursorPoint.y
+            cardRef.current.style.left = `${offsetX}px`
+            cardRef.current.style.top = `${offsetY}px`
+
+            setMoveOffset({
+                x: offsetX,
+                y: offsetY
+            })
         }
+
+        appearByOffset()
     }
 
     const resetCard = () => {
@@ -33,6 +52,37 @@ const Card: React.FC = () => {
             cardRef.current.style.transform = 'rotate(0deg)'
             setPrimaryCursorPoint({ x: 0, y: 0 })
         }
+    }
+
+    const actionByOffset = () => {
+        if (cardRef) {
+            if (moveOffset.x > GOT_IT) {
+                cardRef.current?.style.opacity
+            } else if (moveOffset.x < STUDY_AGAIN) {
+                cardRef.current?.remove()
+            }
+
+            if (moveOffset.y > DELETE_OFFSET) {
+                cardRef.current?.remove()
+            }
+        }
+    }
+
+    const appearByOffset = () => {
+        if (moveOffset.x > GOT_IT && gotItLabelRef.current) {
+            gotItLabelRef.current.style.opacity = normalize(moveOffset.x, GOT_IT, 0).toString()
+            console.log('normalized', normalize(moveOffset.x, GOT_IT, 0).toString())
+        } else if (moveOffset.x < STUDY_AGAIN && studyAgainLabelRef.current) {
+            studyAgainLabelRef.current
+        }
+
+        if (moveOffset.y > DELETE_OFFSET && deleteLabelRef.current) {
+            deleteLabelRef.current
+        }
+    }
+
+    const normalize = (value: number, max: number, min: number): number => {
+        return (value - min) / (max - min)
     }
 
     const onCardClickHandler = () => {
@@ -53,6 +103,7 @@ const Card: React.FC = () => {
 
     const onMouseUpHandler = () => {
         setIsMouseDown(false)
+        actionByOffset()
         resetCard()
     }
 
@@ -77,6 +128,7 @@ const Card: React.FC = () => {
             onMouseDown={onMouseDownHandler}
             onMouseUp={onMouseUpHandler}
             onClick={onCardClickHandler}>
+            { /* content */}
             <div className={styles.Card__content}>
                 {
                     isFrontSide ?
@@ -92,6 +144,22 @@ const Card: React.FC = () => {
                             <h1 className={styles.BackContent__translation}>Long time ago</h1>
                         </div>
                 }
+            </div>
+            { /* action labels */}
+            { /* got it */}
+            <div className={styles.GotItLabel}
+                ref={gotItLabelRef}>
+                <h2 className={styles.ActionLabel__text}>Got it</h2>
+            </div>
+            { /* study again */}
+            <div className={styles.StudyAgainLabel}
+                ref={studyAgainLabelRef}>
+                <h2 className={styles.ActionLabel__text}>Study again</h2>
+            </div>
+            { /* delete */}
+            <div className={styles.DeleteLabel}
+                ref={deleteLabelRef}>
+                <h2 className={styles.ActionLabel__text}>Delete</h2>
             </div>
         </div >
     )
