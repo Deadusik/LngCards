4e9e136594dd70909595dbcc51be5810d06d9ce4
Ruslan <110040060+Deadusik@users.noreply.github.com>
2025-05-20 15:38:11 +0300

    finished deck language dialog component

--- client/src/components/dialog/deck_language/DeckLanguageDialog.tsx
+++ client/src/components/dialog/deck_language/DeckLanguageDialog.tsx
@@ -1,11 +1,11 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import styles from '../../../styles/components/dialog/deck_language/DeckLanguageDialog.module.scss'
 import countries from 'i18n-iso-countries'
 import enLocale from 'i18n-iso-countries/langs/en.json'
 // components
 import LanguageButton from '../app_language/LanguageButton'
 import Search from './Search'
-import { GENERAL_COUNTRY_CODES, SECONDARY_COUNTRY_CODES } from '../../../utils/constants'
+import { COUNTRY_CODES, GENERAL_COUNTRY_CODES, SECONDARY_COUNTRY_CODES } from '../../../utils/constants'
 
 
 interface Props {
@@ -15,9 +15,14 @@ interface Props {
 }
 
 const DeckLanguageDialog: FC<Props> = ({ isHidden, setIsHidden, onSelectedLng }) => {
+    // use state
+    const [searchText, setSearchText] = useState('')
+    const [selectedLanguages, setSelectedLanguages] = useState<[string, string][]>([])
     const dialogStyle = isHidden ? styles.Dialog_hidden : styles.Dialog
+    // language entities
     countries.registerLocale(enLocale)
-    const countryNames = countries.getNames('en')
+    const generalCountriesArr = searchText ? selectedLanguages : Object.entries(GENERAL_COUNTRY_CODES)
+    const secondaryCountriesArr = Object.entries(SECONDARY_COUNTRY_CODES)
 
     const onBgClickHandler = () => {
         setIsHidden(true)
@@ -33,18 +38,35 @@ const DeckLanguageDialog: FC<Props> = ({ isHidden, setIsHidden, onSelectedLng })
     }
 
     const onSearchInputChanged = (text: string) => {
+        setSearchText(text)
 
+        const countryArr = Object.entries(COUNTRY_CODES)
+        const matches: [string, string][] = []
+
+        countryArr.forEach(([code, language]) => {
+            if (language.toLocaleLowerCase().includes(text.toLocaleLowerCase())) {
+                matches.push([code, language])
+            }
+        })
+
+        setSelectedLanguages(matches)
+    }
+
+    const onCloseHandler = () => {
+        setIsHidden(true)
     }
 
     return (
         <div className={dialogStyle} onClick={onBgClickHandler}>
             <div className={styles.Card} onClick={e => onCardClickHandler(e)}>
                 <div className={styles.Card__content}>
-                    <Search onChange={onSearchInputChanged} />
+                    <Search
+                        onChange={onSearchInputChanged}
+                        onClose={onCloseHandler} />
                     <hr className={styles.Card__separator} />
                     <div className={styles.Card__languageBlock}>
                         {
-                            Object.entries(GENERAL_COUNTRY_CODES).map(([code, language]) => (
+                            generalCountriesArr.map(([code, language]) => (
                                 <LanguageButton
                                     key={code}
                                     appLng={{ name: language, countryCode: code.toLowerCase() }}
@@ -52,15 +74,20 @@ const DeckLanguageDialog: FC<Props> = ({ isHidden, setIsHidden, onSelectedLng })
                                 />
                             ))
                         }
-                        <hr className={styles.Card__separator} />
+                        {!searchText && <hr className={styles.Card__separator} />}
                         {
-                            Object.entries(SECONDARY_COUNTRY_CODES).map(([code, language]) => (
+                            !searchText &&
+                            secondaryCountriesArr.map(([code, language]) => (
                                 <LanguageButton
                                     key={code}
                                     appLng={{ name: language, countryCode: code.toLowerCase() }}
                                     onClick={() => onSelectedLngHandler(language, code.toLowerCase())}
                                 />
                             ))
+                        }
+                        {
+                            selectedLanguages.length == 0 &&
+                            <p className={styles.Card__textNoResult}>🤷</p>
                         }
                     </div>
                 </div>
--- client/src/components/dialog/deck_language/Search.tsx
+++ client/src/components/dialog/deck_language/Search.tsx
@@ -4,25 +4,43 @@ import { gray } from '../../../utils/colors'
 import { default as SearchSvg } from '../../../assets/svgs/search.svg?react'
 import { default as CloseSvg } from '../../../assets/svgs/cross.svg?react'
 import { default as DeleteSvg } from '../../../assets/svgs/delete.svg?react'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 interface Props {
     onChange: (text: string) => void
+    onClose: () => void
 }
 
-const Search: FC<Props> = ({ onChange }) => {
+const Search: FC<Props> = ({ onChange, onClose }) => {
+    const [text, setText] = useState('')
+    const inputRef = null
+
+    const onClearHandler = () => {
+        setText('')
+    }
+
+    const onChangeHandler = (text: string) => {
+        setText(text)
+    }
+
+    useEffect(() => {
+        onChange(text)
+    }, [text])
+
     return (
         <div className={styles.Search}>
             <SearchSvg width={20} height={20} stroke={gray} />
             <input
-                onChange={e => onChange(e.target.value)}
+                ref={inputRef}
+                onChange={e => onChangeHandler(e.target.value)}
                 className={styles.Search__input}
                 type='text'
+                value={text}
                 placeholder='Search' />
-            <button className={styles.Search__button}>
+            <button className={styles.Search__button} onClick={onClearHandler}>
                 <DeleteSvg width={30} height={30} fill={gray} />
             </button>
-            <button className={styles.Search__button}>
+            <button className={styles.Search__button} onClick={onClose}>
                 <CloseSvg width={30} height={30} fill={gray} />
             </button>
         </div>
